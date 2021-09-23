c93fa7352449e875f594ec7db33c421e3112d234
a5gallonbucket <typlatt@gmail.com>
2021-09-23 14:17:11 -0700

    naive implementation of ldap client search

---
+++ config.js
@@ -0,0 +1,7 @@
+var config = module.exports = {};
+
+config.ldap_username = "<username>"
+config.ldap_password = "<password>"
+config.ldap_scope ="<scope>"
+config.ldap_filter = "<search_string>"
+config.ldap_uri = ['<URI>']
